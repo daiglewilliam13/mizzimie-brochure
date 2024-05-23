975197b714d7cce92c676538e23a4e0d5f0e9254
William Daigle <daiglewilliam13@gmail.com>
2024-05-23 16:15:34 -0700

    add function to check url params

--- main.js
+++ main.js
@@ -16,20 +16,30 @@ console.log(ofLink);
 const changeLink = (el, newUrl) => {
     el.href=newUrl;
 }
-
 let params = new URLSearchParams(document.location.search);
 let ref = params.get('ref');
+
+const checkRef = (urlRef) => {
+    
+    if (urlRef == "twitter" ) {
+        console.log('source is from twitter');
+        changeLink(ofLink, twitterUrl)
+    } else if (urlRef == "instagram") {
+        console.log('source is from instagram');
+        changeLink(ofLink, igUrl);
+    } else if (urlRef == "tiktok") {
+        console.log('source is from TikTok');
+        changeLink(ofLink, tiktokUrl)
+    } else {
+        console.log('no source found');
+    }
+}
 
 if (sourceUrl == "https://l.instagram.com/") {
     console.log('Source is from instagram: ', sourceUrl );
     changeLink(ofLink, igUrl);
 } else if (sourceUrl == "") {
     console.log('referrer blank, checking URL parameters');
+    checkRef(ref);
 }
-
-if (ref == "twitter" ) {
-    console.log('source is from twitter');
-    changeLink(ofLink, twitterUrl)
-}
-
 
